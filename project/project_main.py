952ca94f9f04e41846738dbca5ab796b89d90303
salimbenghorbel <selim.benghorbal@gmail.com>
2021-05-24 14:29:23 +0200

    Updated project main

--- project/project_main.py
+++ project/project_main.py
@@ -33,28 +33,28 @@ class Robot:
     TILE_OBSTACLE = 2
     TILE_TAKEOFF = 3
     TILE_LANDING = 4
-    
+
     # Robot states
     STATE_EXPLORATION_RIGHT = 0
     STATE_EXPLORATION_LEFT = 1
     STATE_EXPLORATION_RIGHT_BACK = 2
     STATE_EXPLORATION_LEFT_BACK = 3
-    
+
     # 0.1m = 10cm precision
     GRID_PRECISION = 0.1
     DETECTION_THRESHOLD_SIDEWAY = 0.5
     DETECTION_THRESHOLD_Z = 0.5
 
-    TAKEOFF_REGION_X = [0,2]
-    LANDING_REGION_X = [3,6]
-    
+    TAKEOFF_REGION_X = [0, 2]
+    LANDING_REGION_X = [3, 6]
+
     Y_MIN = -1
     Y_MAX = 5
 
-
     def __init__(self, pc, multiranger, x=0, y=0, z=DEFAULT_HEIGHT):
-        ind = pd.MultiIndex.from_arrays([[]] * 2, names=('x','y'))
-        self.dynamic_occupancy = pd.Series(index=ind ,name='occupancy_value',dtype='float64')
+        ind = pd.MultiIndex.from_arrays([[]] * 2, names=('x', 'y'))
+        self.dynamic_occupancy = pd.Series(
+            index=ind, name='occupancy_value', dtype='float64')
         self.x = x
         self.y = y
         self.z = z
@@ -70,24 +70,28 @@ class Robot:
     def fill_dynamic_occupancy(self, x, y, tile_state):
         x_truncated = self.truncate_to_grid_precision(x)
         y_truncated = self.truncate_to_grid_precision(y)
-        
+
         # if element exists, update with new value, else create new item
-        self.dynamic_occupancy.at[x_truncated,y_truncated] = tile_state
+        self.dynamic_occupancy.at[x_truncated, y_truncated] = tile_state
 
     def update_occupancy(self):
         self.x, self.y, self.z = self.pc.get_position()
-        
+
         if self.multiranger.front < self.DETECTION_THRESHOLD_SIDEWAY:
-            self.fill_dynamic_occupancy(self.x+self.multiranger.front,self.y, self.TILE_OBSTACLE)
+            self.fill_dynamic_occupancy(
+                self.x+self.multiranger.front, self.y, self.TILE_OBSTACLE)
 
         if self.multiranger.left < self.DETECTION_THRESHOLD_SIDEWAY:
-            self.fill_dynamic_occupancy(self.x,self.y+self.multiranger.left, self.TILE_OBSTACLE)    
+            self.fill_dynamic_occupancy(
+                self.x, self.y+self.multiranger.left, self.TILE_OBSTACLE)
 
         if self.multiranger.back < self.DETECTION_THRESHOLD_SIDEWAY:
-            self.fill_dynamic_occupancy(self.x-self.multiranger.back,self.y, self.TILE_OBSTACLE)
+            self.fill_dynamic_occupancy(
+                self.x-self.multiranger.back, self.y, self.TILE_OBSTACLE)
 
         if self.multiranger.right < self.DETECTION_THRESHOLD_SIDEWAY:
-            self.fill_dynamic_occupancy(self.x,self.y-self.multiranger.right, self.TILE_OBSTACLE)
+            self.fill_dynamic_occupancy(
+                self.x, self.y-self.multiranger.right, self.TILE_OBSTACLE)
 
         if self.multiranger.down < self.DETECTION_THRESHOLD_Z:
             if self.x <= self.TAKEOFF_REGION_X[0] and self.x >= self.TAKEOFF_REGION_X[1]:
@@ -97,7 +101,6 @@ class Robot:
         else:
             self.fill_dynamic_occupancy(self.x, self.y, self.TILE_FREE)
 
-
     def put_pixel_in_map(self, row):
         x = row['level_0']
         y = row['level_1']
@@ -106,44 +109,46 @@ class Robot:
         if pixel_label == self.TILE_OBSTACLE:
             i = (x - self.x_min)/self.GRID_PRECISION
             j = (y - self.y_min)/self.GRID_PRECISION
-            self.map[i,j] = 255 
-
+            self.map[i, j] = 255
 
     def build_map(self):
 
-        #First create array with detected obstacles.
-        self.x_max = 1 # extract x_max
-        self.x_min = 0 # extract x_min
+        # First create array with detected obstacles.
+        self.x_max = 1  # extract x_max
+        self.x_min = 0  # extract x_min
 
-        self.y_max = 1 # extract y_max
-        self.y_min = 0 # extract y_min
+        self.y_max = 1  # extract y_max
+        self.y_min = 0  # extract y_min
 
-        x_dim = (self.x_max - self.x_min)/self.GRID_PRECISION +1
-        y_dim = (self.y_max - self.y_min)/self.GRID_PRECISION +1
+        x_dim = (self.x_max - self.x_min)/self.GRID_PRECISION + 1
+        y_dim = (self.y_max - self.y_min)/self.GRID_PRECISION + 1
 
         self.map = np.zeros((x_dim, y_dim))
 
-        self.dynamic_occupancy.reset_index().apply(lambda row: self.put_pixel_in_map(row), axis=1)
+        self.dynamic_occupancy.reset_index().apply(
+            lambda row: self.put_pixel_in_map(row), axis=1)
 
-        connected_points, _= cv2.findContours(self.map, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        connected_points, _ = cv2.findContours(
+            self.map, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
         connected_points_m = []
         for row in connected_points:
-            row_m =  []
+            row_m = []
             for pixel in row:
                 x = pixel[0] * self.GRID_PRECISION + self.x_min
                 y = pixel[1] * self.GRID_PRECISION + self.y_min
-                row_m.append([x,y])
+                row_m.append([x, y])
             connected_points_m.append(row_m)
 
         # build obstacles and build visibility graph.
-        obstacles = nav.build_obstacles(connected_points_m, self.GRID_PRECISION)
+        obstacles = nav.build_obstacles(
+            connected_points_m, self.GRID_PRECISION)
         graph = nav.build_visgraph(obstacles)
         # apply A* to extract optimal path and target points coordinates.
-        sp = nav.apply_astar(graph, [self.x, self.y],self.takeoff_position)
+        sp = nav.apply_astar(graph, [self.x, self.y], self.takeoff_position)
         self.all_target_points = []
-        for i in range(0,len(sp)):
-            self.all_target_points.append([sp[i].x,sp[i].y])
+        for i in range(0, len(sp)):
+            self.all_target_points.append([sp[i].x, sp[i].y])
         self.target_point = self.all_target_points[0]
 
     def find_next_target_point(self):
@@ -151,39 +156,37 @@ class Robot:
         find the next target points in the list f all target points
         """
         i = self.all_target_points.index(self.target_point)
-        self.target_point[0] = self.all_target_points[i+1][0]    
+        self.target_point[0] = self.all_target_points[i+1][0]
         self.target_point[1] = self.all_target_points[i+1][1]
 
     def behave_return(self):
         if self.truncate_to_grid_precision(self.x) == self.truncate_to_grid_precision(self.takeoff_position[0]) and \
-            self.truncate_to_grid_precision(self.y) == self.truncate_to_grid_precision(self.takeoff_position[1]):
-            
+                self.truncate_to_grid_precision(self.y) == self.truncate_to_grid_precision(self.takeoff_position[1]):
+
             self.pc.down(self.multiranger.down)
             return False
         elif self.multiranger.front < self.GRID_PRECISION or self.multiranger.back < self.GRID_PRECISION or \
-            self.multiranger.left < self.GRID_PRECISION or self.multiranger.right < self.GRID_PRECISION:
+                self.multiranger.left < self.GRID_PRECISION or self.multiranger.right < self.GRID_PRECISION:
             self.build_map()
         elif self.truncate_to_grid_precision(self.x) == self.truncate_to_grid_precision(self.target_point[0]) and \
-            self.truncate_to_grid_precision(self.y) == self.truncate_to_grid_precision(self.target_point[1]):
+                self.truncate_to_grid_precision(self.y) == self.truncate_to_grid_precision(self.target_point[1]):
             self.find_next_target_point()
         else:
-            self.pc.go_to(self.x + self.GRID_PRECISION * np.sign(self.target_point[0]- self.x), 
-                          self.y + self.GRID_PRECISION * np.sign(self.target_point[1]- self.y))
+            self.pc.go_to(self.x + self.GRID_PRECISION * np.sign(self.target_point[0] - self.x),
+                          self.y + self.GRID_PRECISION * np.sign(self.target_point[1] - self.y))
 
         return True
-
-
 
     def behave_explore(self):
         self.x, self.y, self.z = self.pc.get_position()
 
-        if self.multiranger.down < self.DETECTION_THRESHOLD_Z: 
+        if self.multiranger.down < self.DETECTION_THRESHOLD_Z:
             if self.x <= self.LANDING_REGION_X[0] and self.x >= self.LANDING_REGION_X[1]:
-                self.state = self.STATE_LANDING  
+                self.state = self.STATE_LANDING
         elif self.state == self.STATE_EXPLORATION_RIGHT:
             # map limit reached or obstacle on right sensor
             if self.y < self.Y_MIN or self.multiranger.right < self.GRID_PRECISION:
-                if self.multiranger.front < self.GRID_PRECISION: # if obstacle on front sensor, go left
+                if self.multiranger.front < self.GRID_PRECISION:  # if obstacle on front sensor, go left
                     self.state = self.STATE_EXPLORATION_RIGHT_BACK
                 else:
                     # go forward and switch to left exploration
@@ -192,7 +195,7 @@ class Robot:
             else:
                 self.pc.right(self.GRID_PRECISION)
         elif self.state == self.STATE_EXPLORATION_RIGHT_BACK:
-            if self.multiranger.front > self.GRID_PRECISION: # no obstacle on front sensor
+            if self.multiranger.front > self.GRID_PRECISION:  # no obstacle on front sensor
                 # go forward and switch to left exploration
                 self.pc.forward(self.GRID_PRECISION)
                 self.state = self.STATE_EXPLORATION_LEFT
@@ -201,7 +204,7 @@ class Robot:
         if self.state == self.STATE_EXPLORATION_LEFT:
             # map limit reached or obstacle on left sensor
             if self.y > self.Y_MAX or self.multiranger.left < self.GRID_PRECISION:
-                if self.multiranger.front < self.GRID_PRECISION: # if obstacle on front sensor, go right
+                if self.multiranger.front < self.GRID_PRECISION:  # if obstacle on front sensor, go right
                     self.state = self.STATE_EXPLORATION_LEFT_BACK
                 else:
                     # go forward and switch to right exploration
@@ -210,7 +213,7 @@ class Robot:
             else:
                 self.pc.left(self.GRID_PRECISION)
         elif self.state == self.STATE_EXPLORATION_LEFT_BACK:
-            if self.multiranger.front > self.GRID_PRECISION: # no obstacle on front sensor
+            if self.multiranger.front > self.GRID_PRECISION:  # no obstacle on front sensor
                 # go forward and switch to right exploration
                 self.pc.forward(self.GRID_PRECISION)
                 self.state = self.STATE_EXPLORATION_RIGHT
@@ -218,12 +221,13 @@ class Robot:
                 self.pc.right(self.GRID_PRECISION)
         elif self.state == self.STATE_LANDING:
             self.pc.down(self.multiranger.down)
-            return False 
+            return False
             # end program after landing
-        
+
         # always return true before the landing
         return True
-        
+
+
 def main_sequence():
     with SyncCrazyflie(uri, cf=Crazyflie(rw_cache='./cache')) as scf:
         with PositionHlCommander(
@@ -233,18 +237,15 @@ def main_sequence():
                 default_height=DEFAULT_VELOCITY,
                 controller=PositionHlCommander.CONTROLLER_MELLINGER) as pc:
             with Multiranger(scf) as multiranger:
-                
-                #calibrate yaw
-                pc._hl_commander.go_to(0,0,DEFAULT_HEIGHT,1.57,2)
+
+                # calibrate yaw
+                pc._hl_commander.go_to(0, 0, DEFAULT_HEIGHT, 1.57, 2)
                 time.sleep(2)
                 pc._hl_commander.go_to(0, 0, DEFAULT_HEIGHT, 0, 2)
                 time.sleep(2)
 
-
-                
-                
-                x,y,z = pc.get_position()
-                robot = Robot(pc, multiranger, x,y,z)
+                x, y, z = pc.get_position()
+                robot = Robot(pc, multiranger, x, y, z)
 
                 while robot.behave_explore() == True:
                     robot.update_occupancy()
@@ -253,7 +254,6 @@ def main_sequence():
                 pc.up(DEFAULT_HEIGHT)
 
                 robot.build_map()
-
 
                 while robot.behave_return() == True:
                     robot.update_occupancy()
